0c2e61611acaba82c614eb111db842883c629283
Pramod <pramodhbagh.com>
2023-01-10 16:39:15 +0530

    Data binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -22,48 +22,80 @@ export class AppComponent {
   bRadius = '10px'
   users = ['Pramod', 'Santosh', 'Sam', 'Peter', 'John'];
   usersDetails = [
-    {name: 'Pramod', address: 'Hazaribagh', phone: '9876543210', email: 'pramod@gmail.com'},
-    {name: 'Santosh', address: 'Hyderabad', phone: '9800043210', email: 'santosh@gmail.com'},
-    {name: 'Sam', address: 'Pune', phone: '9876500000', email: 'sam@gmail.com'},
-    {name: 'Peter', address: 'Mumbai', phone: '9876000010', email: 'peter@gmail.com'},
-    {name: 'John', address: 'Bangalore', phone: '90000543210', email: 'john@gmail.com'}
+    { name: 'Pramod', address: 'Hazaribagh', phone: '9876543210', email: 'pramod@gmail.com' },
+    { name: 'Santosh', address: 'Hyderabad', phone: '9800043210', email: 'santosh@gmail.com' },
+    { name: 'Sam', address: 'Pune', phone: '9876500000', email: 'sam@gmail.com' },
+    { name: 'Peter', address: 'Mumbai', phone: '9876000010', email: 'peter@gmail.com' },
+    { name: 'John', address: 'Bangalore', phone: '90000543210', email: 'john@gmail.com' }
   ]
 
   usersDetail = [
-    {name: 'Pramod', address: 'Hazaribagh', phone: '9876543210', email: 'pramod@gmail.com', socialAccount: ['Facebook', 'Instagram', 'Youtube']},
-    {name: 'Santosh', address: 'Hyderabad', phone: '9800043210', email: 'santosh@gmail.com', socialAccount: ['LinkedIn', 'Pinterest', 'Yahoo']},
-    {name: 'Sam', address: 'Pune', phone: '9876500000', email: 'sam@gmail.com', socialAccount: ['Twitter', 'Telegram', 'Yahoo']},
-    {name: 'Peter', address: 'Mumbai', phone: '9876000010', email: 'peter@gmail.com', socialAccount: ['LinkedIn', 'Pinterest', 'Yahoo']},
-    {name: 'John', address: 'Bangalore', phone: '90000543210', email: 'john@gmail.com', socialAccount: ['Facebook', 'Instagram', 'Youtube']}
+    { name: 'Pramod', address: 'Hazaribagh', phone: '9876543210', email: 'pramod@gmail.com', socialAccount: ['Facebook', 'Instagram', 'Youtube'] },
+    { name: 'Santosh', address: 'Hyderabad', phone: '9800043210', email: 'santosh@gmail.com', socialAccount: ['LinkedIn', 'Pinterest', 'Yahoo'] },
+    { name: 'Sam', address: 'Pune', phone: '9876500000', email: 'sam@gmail.com', socialAccount: ['Twitter', 'Telegram', 'Yahoo'] },
+    { name: 'Peter', address: 'Mumbai', phone: '9876000010', email: 'peter@gmail.com', socialAccount: ['LinkedIn', 'Pinterest', 'Yahoo'] },
+    { name: 'John', address: 'Bangalore', phone: '90000543210', email: 'john@gmail.com', socialAccount: ['Facebook', 'Instagram', 'Youtube'] }
   ]
 
-  getName(firstName: String, lastName: String){
+  getName(firstName: String, lastName: String) {
     alert(firstName);
     alert(lastName);
   }
-  getData(value:string){
+  getData(value: string) {
     console.log(value)
   }
-  getInputValue(value:string){
+  getInputValue(value: string) {
     console.log(value);
     this.displayInputValue = value;
   }
-  counter(value:string){
+  counter(value: string) {
     value === 'plus' ? this.count++ : this.count--
   }
-  changeColor(){
+  changeColor() {
     this.textColor = "#ffffff";
     this.bgColor = "red";
     this.bRadius = '30px'
   }
-  formData:any = {}; 
-  getFormData(data:NgForm){
+  formData: any = {};
+  getFormData(data: NgForm) {
     console.log(data)
     this.formData = data;
   }
 
   toggleProp = false;
-  toggleFun(){
+  toggleFun() {
     this.toggleProp = !this.toggleProp;
   }
+
+  todoList: any[] = [];
+  addList(item: string) {
+    this.todoList.push({ id: this.todoList.length + 1, name: item });
+    console.log(this.todoList)
+  }
+  removeList(id: number) {
+    console.log(id)
+    this.todoList = this.todoList.filter(item => item.id !== id)
+    //this.todoList.splice(id, 1)
+  }
+
+
+  //pass data child to parent and parent to child
+  serverElements = [{ type: 'server', name: 'Test Server', content: 'Just a test!' }];
+
+  onServerAdded(serverData: { serverName: string, serverContent: string }) {
+    this.serverElements.push({
+      type: 'server',
+      name: serverData.serverName,
+      content: serverData.serverContent
+    })
+  }
+
+  onBlueprintAdded(blueprintData: { serverName: string, serverContent: string }) {
+    this.serverElements.push({
+      type: 'blueprint',
+      name: blueprintData.serverName,
+      content: blueprintData.serverContent
+    })
+  }
+
 }
